e0d0e1970c33f765d3e979f5b8618af32297ad7c
Lore <lorenyanski@gmail.com>
2026-03-28 18:07:29 +0100

    some random commit idk

--- Scripts/catterpillar_skeletonless.gd
+++ Scripts/catterpillar_skeletonless.gd
@@ -1,3 +1,16 @@
 extends Node2D
 
 @export var segments: Array[Segment]
+
+@onready var cursor: Node2D = $Middle/Cursor
+
+func _physics_process(delta: float) -> void:
+	for segment in segments:
+		var force = (cursor.global_position - segment.global_position) * grip_str() * 20
+		segment.apply_central_force(force)
+
+func grip_str() -> int:
+	var num_grips = 0
+	for segment in segments:
+		if segment.gripped: num_grips += 1
+	return num_grips
