f35d3848c8a569fbcd5a89b629d870979f26cf32
Lore <lorenyanski@gmail.com>
2026-03-28 18:39:44 +0100

    fix

--- Scripts/catterpillar_skeletonless.gd
+++ Scripts/catterpillar_skeletonless.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var segments: Array[Segment]
+@onready var cursor: Node2D = $Middle/Cursor
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -32,12 +33,10 @@ func _physics_process(delta: float) -> void:
 		segments[seg].rotation =rot.angle_to(perpToSurf.normalized())
 		#print(segments[seg].rotation)
 	
-@onready var cursor: Node2D = $Middle/Cursor
-
-func _physics_process(delta: float) -> void:
 	for segment in segments:
 		var force = (cursor.global_position - segment.global_position) * grip_str() * 20
 		segment.apply_central_force(force)
+
 
 func grip_str() -> int:
 	var num_grips = 0
